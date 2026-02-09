d1f6c233bd47d01df36ab4f69dc2cffb522782d9
ambaan2255 <newspotify4444@gmail.com>
2026-02-09 22:59:27 +0530

    ..xxx

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,26 +1,22 @@
 import { DeveloperInfo } from './types';
 
 export const DEV_INFO: DeveloperInfo = {
-  name: "Maurice Gift",
-  title: "Self Learned Developer & Programmer, Upcoming Software Engineer",
+  name: "Anandhu.",
+  title: "Self Learned Developer & Programmer",
   bio: "Crafting scalable digital experiences with tech stack like React, Node.js, and AI. Specialized in distributed systems and high-performance UI components with a focus on enterprise-grade security.",
-  email: "founder@giftedtech.co.ke",
-  github: "github.com/mauricegift",
-  linkedin: "linkedin.com/in/mauricegift",
-  twitter: "twitter.com/GiftedMauriceKe",
-  location: "Eldoret, Kenya",
-  phone: "+254 (799) 916-673", //hey nigga if editing this ensure it's in international format this way for contact form to work well
-  photoUrl: "https://files.giftedtech.co.ke/image/IBmmygifted2.png",
-  instagram: "instagram.com/giftedtechnexus",
-  facebook: "facebook.com/giftedtechnexus",
-  tiktok: "tiktok.com/@giftedtechke",
+  email: "llbser9497@gmail.com",
+  github: "github.com/ambaan2255",
+  location: "kerala, india",
+  wpno: "+917559984214", //hey nigga if editing this ensure it's in international format this way for contact form to work well
+  photoUrl: "https://i.ibb.co/YB6yYcX9/b-AXgcs-XNu5.jpg",
+  instagram: "instagram.com/kiiraa.exe",
   telegram: "t.me/mauricegift",
-  whatsapp: "wa.me/message/OCSOK3IUFPWWA1"
+  whatsapp: "wa.me/919497705819"
 };
 
 export const WEB_INFO = {
-  siteName: "GIFTED",
-  logoUrl: "https://files.giftedtech.co.ke/image/ERNdgifted-md-nobg1.png"
+  siteName: "nova",
+  logoUrl: "https://i.ibb.co/YB6yYcX9/b-AXgcs-XNu5.jpg"
 };
 
 export const STATS = [
